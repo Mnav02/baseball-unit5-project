6eb54cefcf74205518f65d0d1c9a3cd16c45016a
IsaacM22 <91284207+IsaacM22@users.noreply.github.com>
2022-02-11 10:44:23 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let guess = 0
+let value = randint(1, 3)
+game.splash("choose the correct number to play the game")
+while (guess != value) {
+    guess = parseFloat(game.askForString("whats your guess", 1))
+}
+game.splash("nice")
 let car = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . 3 3 3 3 3 3 3 3 . . . . 
@@ -140,21 +147,3 @@ scene.setBackgroundImage(img`
     `)
 controller.moveSprite(car)
 car.setStayInScreen(true)
-let mySprite = sprites.create(img`
-    . . . . f f f f f . . . . . . . 
-    . . . f e e e e e f . . . . . . 
-    . . f d d d d e e e f . . . . . 
-    . c d f d d f d e e f f . . . . 
-    . c d f d d f d e e d d f . . . 
-    c d e e d d d d e e b d c . . . 
-    c d d d d c d d e e b d c . f f 
-    c c c c c d d d e e f c . f e f 
-    . f d d d d d e e f f . . f e f 
-    . . f f f f f e e e e f . f e f 
-    . . . . f e e e e e e e f f e f 
-    . . . f e f f e f e e e e f f . 
-    . . . f e f f e f e e e e f . . 
-    . . . f d b f d b f f e f . . . 
-    . . . f d d c d d b b d f . . . 
-    . . . . f f f f f f f f f . . . 
-    `, SpriteKind.Enemy)
