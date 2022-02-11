d7d6ee03c182bbbda4de7e355094fba683eb9bde
Mnav02 <91284114+Mnav02@users.noreply.github.com>
2022-02-11 10:25:45 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
 let mySprite = sprites.create(img`
-    . . . . f f f f f . . . . . . . 
-    . . . f e e e e e f . . . . . . 
-    . . f d d d d e e e f . . . . . 
-    . c d f d d f d e e f . . . . . 
-    . c d f d d f d e e f f . . . . 
-    c d e e d d d d e e d d f . . . 
-    c d d d d c d d e e b d c . . . 
-    c c c c c d d e e e b d c . f f 
-    . f d d d d e e e f f c . f e f 
-    . f f f f f f e e e e f . f e f 
-    . f f f f e e e e e e e f f e f 
-    f d d f d d f e f e e e e f f . 
-    f d b f d b f e f e e e e f . . 
-    f f f f f f f f f f f f e f . . 
-    . . . . . . . . . f c d d f . . 
-    . . . . . . . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 3 3 3 3 3 3 3 3 . . . . 
+    . . . 3 d 3 3 3 3 3 3 c 3 . . . 
+    . . 3 c d 3 3 3 3 3 3 c c 3 . . 
+    . 3 c c d d d d d d 3 c c d 3 d 
+    . 3 c 3 a a a a a a a b c d 3 3 
+    . 3 3 a b b a b b b a a b d 3 3 
+    . 3 a b b b a b b b b a 3 3 3 3 
+    . a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+    . a a a a a a f a a a f a 3 d d 
+    . a a a a a a f a a f a a a 3 d 
+    . a a a a a a f f f a a a a a a 
+    . a f f f f a a a a f f f a a a 
+    . . f f f f f a a f f f f f a . 
+    . . . f f f . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 scene.setBackgroundImage(img`
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
